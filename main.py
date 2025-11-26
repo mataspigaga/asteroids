2033c9398631dfa08f7e9ae6653c8a014eb94b1a
Matas Pigaga <tech@matas.io>
2025-11-25 19:36:34 -0500

    set FPS to 60 and saved value to dt

--- main.py
+++ main.py
@@ -12,6 +12,9 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     
+    Clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         log_state()
         for event in pygame.event.get():
@@ -20,6 +23,8 @@ def main():
         screen.fill("black")
         pygame.display.flip()
 
+        Clock.tick(60)
+        dt = Clock.tick(60) / 1000
+
 if __name__ == "__main__":
-    
     main()
