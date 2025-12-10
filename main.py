c7e64a7310acc1f1fb53cca98b09e63360d602b5
Matas Pigaga <tech@matas.io>
2025-12-10 15:03:05 -0500

    draw player triangle to screen

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ warnings.filterwarnings("ignore", message=".*pkg_resources is deprecated as an A
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -11,9 +12,11 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
+
     Clock = pygame.time.Clock()
     dt = 0
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         log_state()
@@ -21,10 +24,12 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
 
         Clock.tick(60)
         dt = Clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
---
+++ player.py
@@ -0,0 +1,25 @@
+from constants import PLAYER_RADIUS, LINE_WIDTH
+from circleshape import CircleShape
+from pygame import Vector2, draw
+
+class Player(CircleShape):
+    def __init__(self, x, y):
+        super().__init__(x, y, PLAYER_RADIUS)
+        self.rotation = 0
+
+    # in the Player class
+    def triangle(self):
+        forward = Vector2(0, 1).rotate(self.rotation)
+        right = Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        a = self.position + forward * self.radius
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
+        return [a, b, c]
+    
+    def draw(self, screen):
+        draw.polygon(
+            screen, 
+            "white", 
+            self.triangle(), 
+            LINE_WIDTH
+        )
